aba6b229ac120ec8e4992667c740b036243c464c
MathiasTA <113428638+MathiasTA@users.noreply.github.com>
2025-03-25 13:50:22 +0100

    tæller og sender mål til serial monitor

---
+++ maal.ino
@@ -0,0 +1,36 @@
+const  int BotPin = 4; 
+const int SpillerPin = 2;
+int RobotM = 0;
+int SpillerM = 0;
+
+void setup() {
+  pinMode(BotPin, INPUT);
+  pinMode(SpillerPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int RobotSignal = digitalRead(BotPin);
+  int SpillerSignal = digitalRead(SpillerPin); 
+
+  if (RobotSignal == LOW) {
+    RobotM++;
+    Serial.println("MÅL TIL SPILLEREN");
+    Serial.print("Score: ");
+     Serial.print(RobotM);
+     Serial.print(" : ");
+     Serial.println(SpillerM);
+    delay(1000);
+  } 
+  if (SpillerSignal == LOW) {
+    SpillerM++;
+    Serial.println("MÅL TIL ROBOTTEN");
+
+     Serial.print("Score: ");
+     Serial.print(RobotM);
+     Serial.print(" : ");
+     Serial.println(SpillerM);
+    delay(1000); 
+  } 
+
+}
