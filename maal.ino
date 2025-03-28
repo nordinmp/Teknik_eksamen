9a438d3a48a34216124b7c044357902648df6468
MathiasTA <113428638+MathiasTA@users.noreply.github.com>
2025-03-28 10:00:22 +0100

    nu med minus mål knap

--- maal.ino
+++ maal.ino
@@ -1,5 +1,7 @@
 const  int BotPin = 2; 
 const int SpillerPin = 4;
+const int MinusSpillerPin = 7;
+const int MinusBotPin = 8;
 int RobotM = 0;
 int SpillerM = 0;
 
@@ -30,7 +32,26 @@ void loop() {
      Serial.print(RobotM);
      Serial.print(" : ");
      Serial.println(SpillerM);
-    delay(1000); 
+    delay(1000);
   } 
+  if (digitalRead(MinusBotPin) == HIGH) {
+  RobotM = RobotM - 1;
+  Serial.println("Der blev fjernet et mål fra robotten");
+  Serial.print("Score: ");
+  Serial.print(RobotM);
+  Serial.print(" : ");
+  Serial.println(SpillerM);
+  delay(1000);
+  }
+  if (digitalRead(MinusSpillerPin) == HIGH) {
+  SpillerM = SpillerM - 1;
+  Serial.println("Der blev fjernet et mål fra spilleren");
+  Serial.print("Score: ");
+  Serial.print(RobotM);
+  Serial.print(" : ");
+  Serial.println(SpillerM);
+  delay(1000);
+  }
+  Serial.println(SpillerSignal);
 
 }
