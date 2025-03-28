977c844393fc247a6ec7c2068db764e08477729c
MathiasTA <113428638+MathiasTA@users.noreply.github.com>
2025-03-28 11:09:27 +0100

    viser kun mål der bliver scoret, ikke andet

--- maal.ino
+++ maal.ino
@@ -1,57 +1,71 @@
-const  int BotPin = 2; 
-const int SpillerPin = 4;
+#include <LiquidCrystal.h>
+
+const int rs = 12, en = 11, d4 = 5, d5 = 9, d6 = 3, d7 = 6;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int BotPin = 4;
+const int SpillerPin = 2;
 const int MinusSpillerPin = 7;
 const int MinusBotPin = 8;
 int RobotM = 0;
 int SpillerM = 0;
 
 void setup() {
-  pinMode(BotPin, INPUT);
-  pinMode(SpillerPin, INPUT);
-  Serial.begin(9600);
+    pinMode(BotPin, INPUT);
+    pinMode(SpillerPin, INPUT);
+    pinMode(MinusBotPin, INPUT);
+    pinMode(MinusSpillerPin, INPUT);
+    
+    lcd.begin(16, 2);
+    updateLCD();
 }
 
 void loop() {
-  int RobotSignal = digitalRead(BotPin);
-  int SpillerSignal = digitalRead(SpillerPin); 
+    int RobotSignal = digitalRead(BotPin);
+    int SpillerSignal = digitalRead(SpillerPin);
 
-  if (RobotSignal == LOW) {
-    RobotM++;
-    Serial.println("MÅL TIL ROBOTTEN");
-    Serial.print("Score: ");
-     Serial.print(RobotM);
-     Serial.print(" : ");
-     Serial.println(SpillerM);
+    if (RobotSignal == LOW) {
+        RobotM++;
+        showMessage(RobotM, SpillerM);
+        updateLCD();
+    }
+    
+    if (SpillerSignal == LOW) {
+        SpillerM++;
+        showMessage(RobotM, SpillerM);
+        updateLCD();
+    }
+    
+    if (digitalRead(MinusBotPin) == HIGH && RobotM > 0) {
+        RobotM--;
+        showMessage(RobotM, SpillerM);
+        updateLCD();
+    }
+    
+    if (digitalRead(MinusSpillerPin) == HIGH && SpillerM > 0) {
+        SpillerM--;
+        showMessage(RobotM, SpillerM);
+        updateLCD();
+    }
+}
+
+void updateLCD() {
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("Robot : Spiller");
+    lcd.setCursor(5, 1);
+    lcd.print(RobotM);
+    lcd.print(" : ");
+    lcd.print(SpillerM);
+}
+
+void showMessage(int robotScore, int spillerScore) {
+    lcd.clear();
+    lcd.setCursor(1, 0);
+    lcd.print("Robot : Spiller");
+    lcd.setCursor(5, 1);
+    lcd.print(robotScore);
+    lcd.print(" : ");
+    lcd.print(spillerScore);
     delay(1000);
-  } 
-  if (SpillerSignal == LOW) {
-    SpillerM++;
-    Serial.println("MÅL TIL SPILLEREN");
-
-     Serial.print("Score: ");
-     Serial.print(RobotM);
-     Serial.print(" : ");
-     Serial.println(SpillerM);
-    delay(1000);
-  } 
-  if (digitalRead(MinusBotPin) == HIGH) {
-  RobotM = RobotM - 1;
-  Serial.println("Der blev fjernet et mål fra robotten");
-  Serial.print("Score: ");
-  Serial.print(RobotM);
-  Serial.print(" : ");
-  Serial.println(SpillerM);
-  delay(1000);
-  }
-  if (digitalRead(MinusSpillerPin) == HIGH) {
-  SpillerM = SpillerM - 1;
-  Serial.println("Der blev fjernet et mål fra spilleren");
-  Serial.print("Score: ");
-  Serial.print(RobotM);
-  Serial.print(" : ");
-  Serial.println(SpillerM);
-  delay(1000);
-  }
-  Serial.println(SpillerSignal);
-
 }
